6d260eea56b22129c91989e297a5ef2751ab16fd
Will Bender <willypbender83@gmail.com>
2019-03-13 17:50:06 -0500

    Get the colors down

--- main/simulator.cpp
+++ main/simulator.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <windows.h>
+#include <string>
 // #include <unistd.h>
+
+std::string getColor(int);
 
 int main() 
 {
     while (true) {
-        std::cout << "\033[38;5;208mpeach\033[0;00m\n";
+        std::cout << getColor(0xe67e22) << "\n";
         Sleep(150);
     }
     return 0;
 }
+
+std::string getColor(int hexValue)
+{
+    int red = ((hexValue >> 16) & 0xFF);    // Extract the RR byte
+    int green = ((hexValue >> 8) & 0xFF);   // Extract the GG byte
+    int blue = ((hexValue) & 0xFF);         // Extract the BB byte
+    char buffer[25];
+
+    int n = sprintf(buffer, "\x1b[48;2;%d;%d;%dm \x1b[0m", red, green, blue);
+    return std::string(&buffer[0], n);
+} 
