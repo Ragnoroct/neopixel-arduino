0b1ae117ee8e05675d71ffedc46c118008ec0700
Ragnoroct <willypbender83@gmail.com>
2019-03-15 20:15:09 -0500

    Move justdoit to main

--- main/main.ino
+++ main/main.ino
@@ -5,11 +5,9 @@
 #include <IRremote.h>
 #endif
 
-#include "controllers.h"
-
-#define NUMPIXELS 60 // Number of LEDs in strip
-#define DATAPIN    1
-#define CLOCKPIN   0
+#define NUMPIXELS 30 // Number of LEDs in strip
+#define DATAPIN    3
+#define CLOCKPIN   2
 #define PLAY_PAUSE FFC23D
 
 int IR_Pin = A0;
@@ -19,15 +17,6 @@ int BLUE_LED = 13;
 int RED_LED = 12;
 
 Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-// The last parameter is optional -- this is the color data order of the
-// DotStar strip, which has changed over time in different production runs.
-// Your code just uses R,G,B colors, the library then reassigns as needed.
-// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
-// HealthController healthController = HealthController(&strip);
-
-// Hardware SPI is a little faster, but must be wired to specific pins
-// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
-//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -39,37 +28,69 @@ void setup() {
   pinMode(BLUE_LED, OUTPUT); 
   pinMode(RED_LED, OUTPUT);
   irrecv.enableIRIn(); // Start the receiver
-//  Serial.begin(9600);
+  Serial.begin(9600);
 }
 
-// Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
-
-// int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
-// uint32_t color = 0xFF0000;      // 'On' color (starts red)
-int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
-uint32_t color = 0xFF0000;      // 'On' color (starts red)
+int head  = 0;
 int remoteAction = 0;
 bool isPaused = 0;
 
-void healthLoop() {
-  strip.setPixelColor(head, color); // 'On' pixel at head
-  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
-  strip.show();                     // Refresh strip
-  delay(20);                        // Pause 20 milliseconds (~50 FPS)
-
-  if(++head >= NUMPIXELS) {         // Increment head index.  Off end of strip?
-    head = 0;                       //  Yes, reset head index to start
-    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
-      color = 0xFF0000;             //   Yes, reset to red
+void translateIR(int * remoteAction) // takes action based on IR code received describing Car MP3 IR codes 
+{
+  switch(results.value) {
+    case 0xFFC23D:
+      isPaused ^= 1;  // Play or pause
   }
-  if(++tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
+  delay(500);
+} 
+
+void stripLoop() {
+  updateLightning(head);
+  updateHealth(head);
+  updateCold(head);
+  updateFire(head);
+  updatePoison(head);
+  strip.show();                     // Refresh strip
+  delay(5);                        // Pause 20 milliseconds (~50 FPS)
+
+  if(++head >= NUMPIXELS) head = 0;
 }
 
-void poisonLoop() {
-  for (int i = 0; i < NUMPIXELS; i++) {
-    strip.setPixelColor(i, 0x49B960);
-  }
+int fLightning = 0;
+int eLightning = 5;
+void updateLightning(int pix) {
+   if (pix < fLightning || pix > eLightning) return;
+   strip.setPixelColor(pix, 0x00FFFF);
+}
+
+int fHealth = 6;
+int eHealth = 11;
+void updateHealth(int pix) {
+   if (pix < fHealth || pix > eHealth) return;
+   strip.setPixelColor(pix, 0x00FF00);
+}
+
+int fCold = 12;
+int eCold = 17;
+int coldT = 3;
+int coldCounter = 0;
+void updateCold(int pix) {
+   if (pix < fCold || pix > eCold) return;
+   strip.setPixelColor(pix, 0xFF0000);
+}
+
+int fFire = 18;
+int eFire = 23;
+void updateFire(int pix) {
+   if (pix < fFire || pix > eFire) return;
+   strip.setPixelColor(pix, 0x00FFA5);
+}
+
+int fPoison = 24;
+int ePoison = 29;
+void updatePoison(int pix) {
+   if (pix < fPoison || pix > ePoison) return;
+   strip.setPixelColor(pix, 0x0000FF);
 }
 
 void offLoop() {
@@ -80,48 +101,15 @@ void offLoop() {
 }
 
 void loop() {
-  // int i=0;
-  // if (irrecv.decode(&results)) {
-  //   translateIR(&remoteAction);
-  //   irrecv.resume(); // Receive the next value
-  // }
+   int i=0;
+   if (irrecv.decode(&results)) {
+     translateIR(&remoteAction);
+     irrecv.resume(); // Receive the next value
+   }
 
-  // if (isPaused) {
-  //   offLoop();
-  //   return;
-  // }
-    
-//    ir = analogRead(IR_Pin); 
-//  strip.setBrightness(1);
-  strip.setPixelColor(1, 0xFFFFFF);
-  strip.show();
-  // healthController.loop();
-  // Serial.println(healthController);
-  // healthLoop();
-//  IR_Value = analogRead(IR_Pin);
-  // healthLoop();
-//  Serial.println("delay value for LED = "+ IR_Value);
-//  strip.setPixelColor(head, color); // 'On' pixel at head
-//  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
-//  strip.show();                     // Refresh strip
-//  delay(20);                        // Pause 20 milliseconds (~50 FPS)
-//
-//  if(++head >= NUMPIXELS) {         // Increment head index.  Off end of strip?
-//    head = 0;                       //  Yes, reset head index to start
-//    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
-//      color = 0xFF0000;             //   Yes, reset to red
-//  }
-//  if(++tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
+   if (isPaused) {
+    offLoop();
+    return;
+   }
+  stripLoop();
 }
-
-void translateIR(int * remoteAction) // takes action based on IR code received describing Car MP3 IR codes 
-{
-  switch(results.value) {
-    case 0xFFC23D:
-      isPaused ^= 1;  // Play or pause
-  }
-
-  delay(500);
-
-
-} 
